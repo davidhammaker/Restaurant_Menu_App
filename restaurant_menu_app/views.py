8f0f3b823af280e78ae5e6b4b03ad26903d97807
David J. Hammaker <davidjosh93@gmail.com>
2018-12-07 17:17:43 -0500

    Add new 'restaurant' view
    View allows users to navigate to individual restaurant pages, each of
    which displays a full menu with names, prices, courses, and
    descriptions.

--- restaurant_menu_app/views.py
+++ restaurant_menu_app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from restaurant_menu_app import app
-from restaurant_menu_app.models import Restaurant
+from restaurant_menu_app.models import Restaurant, MenuItem
 
 
 @app.route('/')
@@ -8,3 +8,12 @@ from restaurant_menu_app.models import Restaurant
 def home():
     restaurants = Restaurant.query.all()
     return render_template('home.html', restaurants=restaurants)
+
+
+@app.route('/<string:restaurant_name>')
+def restaurant(restaurant_name):
+    restaurant = Restaurant.query.filter_by(name=restaurant_name).first()
+    if not restaurant:
+        return redirect(url_for('home'))
+    items = restaurant.menu_items
+    return render_template('restaurant.html', restaurant=restaurant, items=items)
