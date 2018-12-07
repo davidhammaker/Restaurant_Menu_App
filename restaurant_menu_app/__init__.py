4b6df39f5ab27f4d163bfd995f5435f20c9238cc
David J. Hammaker <davidjosh93@gmail.com>
2018-12-07 15:59:23 -0500

    Reorganize the app into a package
    The package directory is 'restaurant_menu_app'. Within the package
    direcory, imports and configurations have been moved to '__init__.py'.
    Views are now in 'views.py'. The 'templates' and 'static' directories
    have also been moved to the package directory. The database is not
    included in the repository, but it has also been moved to the package.
    Outside the package, 'run.py' contains the 'if __name__ == '__main__'
    conditional.

--- restaurant_menu_app/__init__.py
+++ restaurant_menu_app/__init__.py
@@ -2,17 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from restaurant_menu_app import views
