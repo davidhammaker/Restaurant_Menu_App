a2e9f23fce8bc3898a9f896426161db621495916
David J. Hammaker <davidjosh93@gmail.com>
2018-12-07 16:28:31 -0500

    Add restaurants to 'home'
    The 'home' view now returns restaurants when it renders the template,
    and the template displays all restaurants in an arbitrary order. The
    stylesheet has been updated to account for the change.

--- restaurant_menu_app/views.py
+++ restaurant_menu_app/views.py
@@ -1,8 +1,12 @@
-from flask import render_template
+from flask import render_template, url_for
 from restaurant_menu_app import app
+from restaurant_menu_app.models import Restaurant
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    restaurants = Restaurant.query.all()
+    for restaurant in restaurants:
+        print(restaurant.name)
+    return render_template('home.html', restaurants=restaurants)
