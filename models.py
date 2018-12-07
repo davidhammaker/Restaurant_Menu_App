7122479646c913e2821c3aaf0364bafadb86875f
David J. Hammaker <davidjosh93@gmail.com>
2018-12-07 15:45:37 -0500

    Configure database
    Configure the existing 'restaurants.db' database to work with the Flask
    app.

---
+++ models.py
@@ -0,0 +1,16 @@
+from restaurant_menu_app import db
+
+
+class Restaurant(db.Model):
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    menu_items = db.relationship('MenuItem', backref='restaurant', lazy=True)
+
+
+class MenuItem(db.Model):
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    price = db.Column(db.String(8))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
--- restaurant_menu_app.py
+++ restaurant_menu_app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
