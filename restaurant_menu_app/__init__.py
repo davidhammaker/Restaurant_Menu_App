552974ff4b3bbb1811933a6408e03026fbc20b55
David J. Hammaker <davidjosh93@gmail.com>
2018-12-07 20:45:31 -0500

    Add secret key configuration to '__init__.py'
    Actual key is an environment variable.

--- restaurant_menu_app/__init__.py
+++ restaurant_menu_app/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('RESTAURANT_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
