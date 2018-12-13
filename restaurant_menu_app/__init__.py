6426bc97a27a61c1adfdac9763464e38a7b5a6ab
David J. Hammaker <davidjosh93@gmail.com>
2018-12-13 16:09:33 -0500

    Switch to App Factory Pattern
    Create 'config.py' with configurations, which includes an environment
    variable for the database URI instead of a hard-coded URI. The new
    'create_app' function in '__init__.py' initializes and configures the
    app, initializes extensions, registers blueprints, and returns the app.

--- restaurant_menu_app/__init__.py
+++ restaurant_menu_app/__init__.py
@@ -2,17 +2,22 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('RESTAURANT_SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-from restaurant_menu_app.items.views import items
-from restaurant_menu_app.jsons.views import jsons
-from restaurant_menu_app.main.views import main
-from restaurant_menu_app.restaurants.views import restaurants
-app.register_blueprint(items)
-app.register_blueprint(jsons)
-app.register_blueprint(main)
-app.register_blueprint(restaurants)
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+
+    from restaurant_menu_app.items.views import items
+    from restaurant_menu_app.jsons.views import jsons
+    from restaurant_menu_app.main.views import main
+    from restaurant_menu_app.restaurants.views import restaurants
+    app.register_blueprint(items)
+    app.register_blueprint(jsons)
+    app.register_blueprint(main)
+    app.register_blueprint(restaurants)
+
+    return app
