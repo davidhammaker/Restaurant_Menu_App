8eb45378defa5d4611a3c34c7c2683b6a6ac833c
David J. Hammaker <davidjosh93@gmail.com>
2018-12-13 16:15:50 -0500

    Create and initialize login manager
    This will include the LoginManager and correct login view (which has not 
    yet been created).

--- restaurant_menu_app/__init__.py
+++ restaurant_menu_app/__init__.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from restaurant_menu_app.config import Config
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'github.login'
 
 
 def create_app(config_class=Config):
@@ -11,6 +15,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from restaurant_menu_app.items.views import items
     from restaurant_menu_app.jsons.views import jsons
