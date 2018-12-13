a67f54d262515111dcfa2cfae01ef960a6e8db30
David J. Hammaker <davidjosh93@gmail.com>
2018-12-13 17:27:31 -0500

    Set up SQLAlchemy backend for Flask-Dance
    Allows OAuth tokens to be stored in the database for each user.

--- restaurant_menu_app/dance/views.py
+++ restaurant_menu_app/dance/views.py
@@ -1,10 +1,14 @@
 import os
 from flask import url_for
 from flask_dance.contrib.github import make_github_blueprint, github
+from flask_dance.consumer.backend.sqla import SQLAlchemyBackend
+from flask_login import current_user
+from restaurant_menu_app import db
+from restaurant_menu_app.models import OAuth
 
 blueprint = make_github_blueprint(client_id=os.environ.get("GH_CLIENT_ID"),
                                   client_secret=os.environ.get("GH_CLIENT_SECRET"))
 
-
+blueprint.backend = SQLAlchemyBackend(OAuth, db.session, user=current_user)
 
 
