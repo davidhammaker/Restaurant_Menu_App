d9243491b77837f2279f0936b240d0cf843de790
David J. Hammaker <davidjosh93@gmail.com>
2018-12-13 17:53:01 -0500

    Enable create/login local user on successful login
    This function closely mirrors the function in the Flask-Dance
    documentation for the SQLAlchemy Multi-User Quickstart.

--- restaurant_menu_app/dance/views.py
+++ restaurant_menu_app/dance/views.py
@@ -1,10 +1,12 @@
 import os
-from flask import url_for
+from flask import url_for, flash
+from sqlalchemy.orm.exc import NoResultFound
 from flask_dance.contrib.github import make_github_blueprint, github
 from flask_dance.consumer.backend.sqla import SQLAlchemyBackend
-from flask_login import current_user
+from flask_dance.consumer import oauth_authorized
+from flask_login import current_user, login_user
 from restaurant_menu_app import db
-from restaurant_menu_app.models import OAuth
+from restaurant_menu_app.models import OAuth, User
 
 blueprint = make_github_blueprint(client_id=os.environ.get("GH_CLIENT_ID"),
                                   client_secret=os.environ.get("GH_CLIENT_SECRET"))
@@ -12,3 +14,34 @@ blueprint = make_github_blueprint(client_id=os.environ.get("GH_CLIENT_ID"),
 blueprint.backend = SQLAlchemyBackend(OAuth, db.session, user=current_user)
 
 
+@oauth_authorized.connect_via(blueprint)
+def github_logged_in(blueprint, token):
+    if not token:
+        flash("Failed to log in with GitHub.", "bad")
+        return False
+
+    resp = blueprint.session.get('/user')
+    if not resp.ok:
+        flash("Failed to fetch user info from GitHub", "bad")
+        return False
+
+    github_info = resp.json()
+    github_user_id = str(github_info["id"])
+
+    query = OAuth.query.filter_by(provider=blueprint.name, provider_user_id=github_user_id, token=token)
+    try:
+        oauth = query.one()
+    except NoResultFound:
+        oauth = OAuth(provider=blueprint.name, provider_user_id=github_user_id, token=token)
+
+    if oauth.user:
+        login_user(oauth.user)
+        flash("Successfully signed in with GitHub!", "good")
+    else:
+        user = User(email=github_info["email"], name=github_info["name"], username=github_info["login"])
+        oauth.user = user
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        login_user(user)
+        flash("Successfully signed in with GitHub!", "good")
+    return False
