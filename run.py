7dff81e89e6274323f227879f70e587b89c36ef3
David J. Hammaker <davidjosh93@gmail.com>
2018-12-13 22:43:09 -0500

    Enable 'run.py' to set up tables
    Running `$ python run.py --setup` will now set up the database tables.

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import sys
 from restaurant_menu_app import create_app, db
 
 
 if __name__ == '__main__':
     app = create_app()
+    if "--setup" in sys.argv:
+        with app.app_context():
+            db.create_all()
+            db.session.commit()
     app.run(debug=True)
